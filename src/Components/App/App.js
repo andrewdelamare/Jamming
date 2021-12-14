ab93e58b116ff35b70e1813330a248c79069665f
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2021-12-14 14:11:22 +0200

    Search bar setup

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div>
-        <h1>Ja<span class="highlight">mmm</span>ing</h1>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
         [// Add a SearchBar component]
         <div className="App-playlist">
---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './SearchBar.css'
+export class searchBar extends React.Component {
+    render(){
+        return (
+            <div className="SearchBar">
+                <input placeholder="Enter A Song, Album, or Artist" />
+                <button className="SearchButton">SEARCH</button>
+            </div>
+        )
+    }
+}

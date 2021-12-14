85fa17349785c2aa2da574e24e9e5681063c2fbd
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2021-12-14 14:53:00 +0200

    Track setup

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -4,7 +4,7 @@ import React from "react";
 export class Playlist extends React.Component{
     render(){
         return(
-            <div class="Playlist">
+            <div className="Playlist">
                 <input defaultValue={'New Playlist'}/>
                  {/* Add a TrackList component */}
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>

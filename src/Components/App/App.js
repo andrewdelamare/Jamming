fbaf216455b32fb80d04148ce5db9f650edcb7ee
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2021-12-14 17:35:51 +0200

    starting work on playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,12 +7,19 @@ import SearchResults from 'src/Components/SearchResults/SearchResults.js';
 class App extends React.Component {
     constructor(props){
     super(props);
-    this.state = {searchResults: [
+    this.state = {
+      searchResults: [
       {name: 'song', artist: 'noOne', album: 'blank', id: '01'}, 
       {name: 'song2', artist: 'no2', album: 'blank2', id: '02'}, 
       {name: 'song3', artist: 'no3', album: 'blank3', id: '03'}, 
-      {name: 'song4', artist: 'no4', album: 'blank4', id: '04'}]};
-
+      {name: 'song4', artist: 'no4', album: 'blank4', id: '04'}],
+      playlistName: 'fakesongs for no one',
+      playlistTracks: [
+        {name: 'song', artist: 'noOne', album: 'blank', id: '01'}, 
+        {name: 'song2', artist: 'no2', album: 'blank2', id: '02'}, 
+        {name: 'song3', artist: 'no3', album: 'blank3', id: '03'}, 
+        {name: 'song4', artist: 'no4', album: 'blank4', id: '04'}]
+    };
   }
 
   render(){
@@ -23,7 +30,7 @@ class App extends React.Component {
           {/* Add a SearchBar component */}
             <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} />
-            {/* Add a Playlist component */}
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
           </div>
         </div>
       </div>
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,12 +1,12 @@
 import React from "react";
-import TrackList 'src/Components/TrackList/TrackList.js';
+import TrackList from 'src/Components/TrackList/TrackList.js';
 import 'src/Components/Playlist/Playlist.css';
 export class Playlist extends React.Component{
     render(){
         return(
             <div className="Playlist">
                 <input defaultValue={'New Playlist'}/>
-                 {/* Add a TrackList component */}
+                 <TrackList tracks={this.props.playlistTracks}/>
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         );

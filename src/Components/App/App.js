bc49791e7f9e10d06837d24904a664c646869bfe
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2021-12-15 09:05:19 +0200

    add track now works

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -15,18 +15,29 @@ export class App extends React.Component {
       {name: 'song4', artist: 'no4', album: 'blank4', id: '04'}],
       playlistName: 'fakesongs for no one',
       playlistTracks: [
-        {name: 'song', artist: 'noOne', album: 'blank', id: '01'}, 
-        {name: 'song2', artist: 'no2', album: 'blank2', id: '02'}, 
-        {name: 'song3', artist: 'no3', album: 'blank3', id: '03'}, 
-        {name: 'song4', artist: 'no4', album: 'blank4', id: '04'}]
+        {name: 'song5', artist: 'no5', album: 'blank', id: '05'}, 
+        {name: 'song6', artist: 'no6', album: 'blank', id: '06'}, 
+        {name: 'song7', artist: 'no7', album: 'blank', id: '07'}, 
+        {name: 'song8', artist: 'no8', album: 'blank', id: '08'}]
     };
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
   addTrack(track){
-    if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)){
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === track.id)){
       return;
     }else{
-      this.state.playlistTracks.push(track);
+      tracks.push(track);
+      this.setState({playlistTracks: tracks});
+    }
+  }
+  removeTrack(track){
+    if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)){
+      let index = this.state.playlistTracks.findIndex(track);
+      this.setState(this.playlistTracks.splice(index,1));
+    }else{
+      return;
     }
   }
   render(){
@@ -36,8 +47,8 @@ export class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-          <SearchResults searchResults={this.state.searchResults} onAdd={this.state.playlistTracks}/>
-          <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks}/>
+          <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack}/>
+          <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack}/>
           </div>
         </div>
       </div>
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -3,12 +3,18 @@ import './Track.css';
 export class Track extends React.Component{
     constructor(props){
         super(props);
-        this.renderAction = this.renderAction.bind(this);
+        this.addTrack = this.addTrack.bind(this);
     }
     renderAction() {
-        if (this.props.isRemoval){ return <button className="Track-action">-</button>;
-        }else{ return <button className="Track-action" onClick={this.addTrack}>+</button>};
+        if (this.props.isRemoval){ 
+            return <button className="Track-action">-</button>;
+        }else{ 
+            return <button className="Track-action" onClick={this.addTrack}>+</button>
         }
+    }
+    addTrack() {
+        this.props.onAdd(this.props.track);
+    }
     render(){
         return(
             <div className="Track">
